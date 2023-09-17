dcca1a4447b08480c69c3a500242795182e29bb4
zeel dhorajiya <zeeldhorajiya@gmail.com>
2023-09-17 18:00:35 +0530

    Ai - Android Version Detection

--- CommonAi/src/main/java/com/zeel_enterprise/commonai/Ai.kt
+++ CommonAi/src/main/java/com/zeel_enterprise/commonai/Ai.kt
@@ -2,13 +2,22 @@ package com.zeel_enterprise.commonai
 
 import android.app.Activity
 import android.content.Intent
+import android.os.Build
 
 class Ai {
 
     companion object {
 
-        fun start(context: Activity, clazz: Class<*>) {
-            context.startActivity(Intent(context, clazz))
+        fun start(context : Activity , clazz : Class<*>) {
+            context.startActivity(Intent(context , clazz))
+        }
+
+        fun isBelowR() : Boolean {
+            return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R).not()
+        }
+
+        fun isTiramisu() : Boolean {
+            return (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU)
         }
 
     }
